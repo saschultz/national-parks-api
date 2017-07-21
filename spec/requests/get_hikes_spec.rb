f86eb89f0250d6507107d7be1a2a9a0f743092d7
Sara Schultz <saschultz95@gmail.com>
2017-07-21 15:59:17 -0700

    pass http status spec for get_hikes_spec.rb

--- spec/requests/get_hikes_spec.rb
+++ spec/requests/get_hikes_spec.rb
@@ -12,4 +12,7 @@ describe 'get hikes route', :type => :request do
     expect(JSON.parse(response.body).size).to eq 1
   end
 
+  it 'returns status code 200' do
+    expect(response).to have_http_status :success
+  end
 end
