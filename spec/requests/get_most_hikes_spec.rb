9dfeaa095ae260d1d86c36fa2b19f34c878dafb3
Sara Schultz <saschultz95@gmail.com>
2017-07-21 16:40:53 -0700

    pass http status spec for most_hikes route

--- spec/requests/get_most_hikes_spec.rb
+++ spec/requests/get_most_hikes_spec.rb
@@ -12,4 +12,8 @@ describe "get most_hikes route", :type => :request do
     expect(JSON.parse(response.body).size).to eq 1
   end
 
+  it 'returns status code 200' do
+    expect(response).to have_http_status :success
+  end
+
 end
