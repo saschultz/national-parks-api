6576c2d67c68a7dc2a7a4284c3eb2fdbf0ccda22
Sara Schultz <saschultz95@gmail.com>
2017-07-21 16:16:03 -0700

    pass response message for patch_hike spec

---
+++ spec/requests/patch_hike_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "patch hikes route", :type => :request do
+
+  before do
+    park = Park.create(name: 'Glacier', location: 'Montana', free_entrance_days: 'Everyday!', id: 1)
+    hike = Hike.create(name: 'Best Hike Ever', miles: 8.1, difficulty: 'medium', park_id: 1)
+    patch "/parks/#{park.id}/hikes/#{hike.id}", params: { name: 'Best Hike Ever, seriously' }
+  end
+
+  it 'returns update message' do
+    expect(JSON.parse(response.body)['message']).to eq "Hike successfully updated."
+  end
+
+end
