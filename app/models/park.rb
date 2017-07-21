d9525ac37bff592df032d26b51bc3da74f0b58fe
Sara Schultz <saschultz95@gmail.com>
2017-07-21 15:07:30 -0700

    add scope for most hikes in park.rb

--- app/models/park.rb
+++ app/models/park.rb
@@ -5,4 +5,12 @@ class Park < ApplicationRecord
   def self.search(input)
     self.find_by(location: input)
   end
+
+  scope :most_hikes, -> {(
+    select("parks.id, parks.name, count(hikes.id) as hikes_count")
+    .joins(:hikes)
+    .group('parks.id')
+    .order('hikes_count DESC')
+    .limit(3)
+    )}
 end
