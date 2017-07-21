9626b2f7e3774bdd2c1c15da0e40c248d419c0d2
Sara Schultz <saschultz95@gmail.com>
2017-07-21 11:45:28 -0700

    pass spec for validating presence of name in park_spec.rb

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,3 +1,4 @@
 class Park < ApplicationRecord
   has_many :attractions, dependent: :destroy
+  validates(:name, presence: true)
 end
--- spec/models/park_spec.rb
+++ spec/models/park_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe Park do
   it { should have_many :attractions }
+  it { should validate_presence_of :name }
 end
