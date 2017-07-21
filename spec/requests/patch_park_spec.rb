6a91e3c0d4963b73c545d9abfe4a6cf5b0dfaec9
Sara Schultz <saschultz95@gmail.com>
2017-07-21 16:12:56 -0700

    pass http status spec in patch_park_spec.rb

--- spec/requests/patch_park_spec.rb
+++ spec/requests/patch_park_spec.rb
@@ -11,5 +11,9 @@ describe "patch parks route", :type => :request do
     expect(JSON.parse(response.body)['message']).to eq "Park was successfully updated."
   end
 
+  it 'returns status code 200' do
+    expect(response).to have_http_status :success
+  end
+
 
 end
