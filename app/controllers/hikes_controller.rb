d5e4eedb53619d27a1b0efb94a8ed71dfeaaf7d8
Sara Schultz <saschultz95@gmail.com>
2017-07-21 14:25:32 -0700

    correct id error in index block of hikes controller

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -1,9 +1,14 @@
 class HikesController < ApplicationController
 
   def index
-    @park = Park.find(params[:id])
-    json_response(@park.reviews)
+    @park = Park.find(params[:park_id])
+    json_response(@park.hikes)
   end
+
+  # def show
+  #   @hike = Hike.find(params[:id])
+  #   json_response(@hike)
+  # end
 
   private
   def json_response(object, status = :ok)
