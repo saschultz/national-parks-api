b697dbb513e42de1e37a889a114bd8e7bee66d07
Sara Schultz <saschultz95@gmail.com>
2017-07-21 12:12:59 -0700

    rollback database to remove attractions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170721181445) do
+ActiveRecord::Schema.define(version: 20170721175741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "attractions", force: :cascade do |t|
-    t.string "hikes"
-    t.string "wildlife"
-    t.string "natural_wonders"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "park_id"
-  end
 
   create_table "parks", force: :cascade do |t|
     t.string "name"
