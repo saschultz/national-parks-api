a3ff6e445c73672dd36376040b91af0dbf109657
Sara Schultz <saschultz95@gmail.com>
2017-07-21 14:55:31 -0700

    define search by location method in park.rb

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,4 +1,8 @@
 class Park < ApplicationRecord
   has_many :hikes, dependent: :destroy
   validates(:name, :location, :free_entrance_days, presence: true)
+
+  def self.search(input)
+    self.find_by(location: input)
+  end
 end
