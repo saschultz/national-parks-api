9b31bab975b91e2139d48c4f027f0e9c7dfd2a22
Sara Schultz <saschultz95@gmail.com>
2017-07-21 16:57:33 -0700

    pass response spec for get park by id test

---
+++ spec/requests/get_park_by_id_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'get park by id route', :type => :request do
+
+  before do
+    park = Park.create(name: 'Park', location: 'Oregon', free_entrance_days: 'Everyday!', id: 2)
+    get '/parks/2'
+  end
+
+  it 'returns one park' do
+    expect(JSON.parse(response.body)['location']).to eq 'Oregon'
+  end
+
+end
