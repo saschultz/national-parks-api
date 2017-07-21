fa07aada77437d010ee8eb06d0b00569f8c5eaf8
Sara Schultz <saschultz95@gmail.com>
2017-07-21 14:32:00 -0700

    add functionality to POST a hike in hike controller

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -10,9 +10,19 @@ class HikesController < ApplicationController
     json_response(@hike)
   end
 
+  def create
+    @park = Park.find(params[:park_id])
+    @hike = @park.hikes.create!(hike_params)
+    json_response(@hike.park, :created)
+  end
+
   private
   def json_response(object, status = :ok)
     render json: object, status: status
   end
+
+  def hike_params
+    params.permit(:name, :miles, :difficulty)
+  end  
 
 end
